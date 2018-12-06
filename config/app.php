46f5fd65c6af09960cdac170c94c2f1573124758
mohammedsehweil <sehweil@souktel.org>
2018-12-06 11:39:45 +0200

    fix

--- config/app.php
+++ config/app.php
@@ -185,7 +185,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\ObserverServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        '\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider'
+
     ],
 
 
