57c1df6caeefaba0ad947f7fb78cc989f04a39ac
mohammedsehweil <sehweil@souktel.org>
2018-12-06 02:03:10 +0200

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -52,8 +52,6 @@ $app->singleton(
 |
 */
 
-if($app->environment() !== 'production') {
-    $app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-}
+$app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
 
 return $app;
