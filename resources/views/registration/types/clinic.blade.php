2541517789be50b5182f991639db2f73be882b8d
mohammedsehweil <sehweil@souktel.org>
2019-02-24 09:56:35 +0200

    Registration form.

--- resources/views/registration/types/clinic.blade.php
+++ resources/views/registration/types/clinic.blade.php
@@ -6,17 +6,62 @@
     <div class="card">
         <div class="card-body">
             <div class="row">
-                <div class="col-sm-5">
+                <div class="col-md-5">
                     <h4 class="card-title mb-0">
                         Clinic Registration
                     </h4>
                 </div>
             </div>
             <br>
-            <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong>Patient</strong>
+                            <small>Personal Info</small>
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input class="form-control" id="name" type="text" placeholder="Enter your Name" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="phone">Phone Number</label>
+                                <input class="form-control" id="phone" type="text"
+                                       placeholder="Enter your phone number" required>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong>Registration</strong>
+                            <small>Form</small>
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group">
+
+                            </div>
+                            <div class="form-group">
+                                <label for="phone">Phone Number</label>
+                                <input class="form-control" id="phone" type="text"
+                                       placeholder="Enter your phone number" required>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 
             </div>
+
+
+
         </div>
     </div>
 
